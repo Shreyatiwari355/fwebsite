c2597497861ea8a5d9974805caca4a3334311822
Shreya <tiwarishreya355@gmail.com>
2022-03-31 20:29:10 +0530

    menu

--- menu.js
+++ menu.js
@@ -1,6 +1,7 @@
 const hamburger=document.getElementById('hamburger');
-const navUl=document.getElementById('navbar ul');
-
+const navUl=document.getElementById('navbar-ul');
+console.log(hamburger);
 hamburger.addEventListener('click',()=>{
+    
     navUl.classList.toggle('show');
 })
